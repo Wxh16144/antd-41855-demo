d2507eadab0b9dc89ea0991ee02f6c606b139087
wuxh <wxh1220@gmail.com>
2023-07-17 18:22:45 +0800

    feat: test css compatibility program

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App as AntdApp, ConfigProvider } from "antd";
+import {
+  StyleProvider,
+  px2remTransformer,
+  legacyLogicalPropertiesTransformer
+} from '@ant-design/cssinjs';
 import EscapeAntd from "./shared/EscapeAntd";
 import App from "./App";
 import "./index.css";
 
+const px2rem = px2remTransformer({
+  rootValue: 8, // 32px = 1rem; @default 16
+});
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <ConfigProvider theme={{}}>
-    <AntdApp>
-      <EscapeAntd />
-      <App />
-    </AntdApp>
-  </ConfigProvider>
+  <StyleProvider transformers={[px2rem, legacyLogicalPropertiesTransformer]}>
+    <ConfigProvider theme={{}}>
+      <AntdApp>
+        <EscapeAntd />
+        <App />
+      </AntdApp>
+    </ConfigProvider>
+  </StyleProvider>
 );
